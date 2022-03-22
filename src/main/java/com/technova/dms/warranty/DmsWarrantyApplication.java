6a60b3f6c39509883dcac50fefb64171d4eae918
Siddhant Thapa Chhetry <90285183+605797@users.noreply.github.com>
2022-03-21 22:17:52 -0500

    added list to the toLear.txt file

--- src/main/java/com/technova/dms/warranty/DmsWarrantyApplication.java
+++ src/main/java/com/technova/dms/warranty/DmsWarrantyApplication.java
@@ -2,8 +2,10 @@ package com.technova.dms.warranty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.technova.dms.warranty")
 public class DmsWarrantyApplication {
 
 	public static void main(String[] args) {
